f47c330abede43dcac48aaaad2ff23464ba5a21f
spook23q <ghostkay187@gmail.com>
2026-03-30 13:40:18 +1100

    chore: clean .gitignore + remove system files

--- 4P/4paws_final/metro.config.cjs
+++ 4P/4paws_final/metro.config.cjs
@@ -1,22 +1,6 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+// metro.config.cjs
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Force development mode for unminified errors
-config.transformer = {
-  ...config.transformer,
-  minifierConfig: {
-    ...config.transformer?.minifierConfig,
-    keep_classnames: true,
-    keep_fnames: true,
-    mangle: false,
-  },
-};
-
-module.exports = withNativeWind(config, {
-  input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
-  forceWriteFileSystem: true,
-});
+module.exports = config;
